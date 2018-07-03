709b77404c97201219795e0463fb75d4bfebbbfb
Jesse Garcia <skaterjess2012@gmail.com>
2018-07-03 08:33:08 -0600

    removed package-lock.json and added a few new lines to the FBservice

--- src/providers/firebase-db/firebase-db.ts
+++ src/providers/firebase-db/firebase-db.ts
@@ -1,15 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { componentFactoryName } from '@angular/compiler';
 
-/*
-  Generated class for the FirebaseDbProvider provider.
+export interface Item { name: string; }
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
-@Injectable()
+@Component({
+  selector: 'app-root',
+  template: `
+    <ul>
+      <li *ngFor="let item of items | async">
+        {{ item.name }}
+      </li>
+    </ul>
+  `
+})
 export class FirebaseDbProvider {
-
-  constructor(public http: HttpClient) {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  path: string = '';
+  constructor(public http: HttpClient, private afs: AngularFirestore) {
+    if (this.path.length > 1) {
+      this.itemsCollection = afs.collection<Item>(this.path);
+      this.items = this.itemsCollection.valueChanges();
+    } else {
+      console.log('WARNING Need to define a path in the firebase-db provider!');
+    }
+  }
+  addItem(item: Item) {
+    this.itemsCollection.add(item);
   }
 }
