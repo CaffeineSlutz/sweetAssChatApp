7494a0671f41afc9834990f0ff00545f52479d93
meganmuirhead <megan@s56.net>
2018-07-17 21:05:47 -0600

    my last push... maybe

--- src/providers/firebase-db/firebase-db.ts
+++ src/providers/firebase-db/firebase-db.ts
@@ -4,10 +4,13 @@ import { Observable } from 'rxjs';
 import { User } from '../../interfaces/user';
 import * as firebase from 'firebase/app'
 import 'rxjs/add/operator/debounceTime';
+import {firestore} from "firebase";
 
 @Injectable()
 export class FirebaseDbProvider {
   users: any = [];
+  foundUser;
+  newThread: any = [];
 
   constructor(private afs: AngularFirestore) {
   }
@@ -20,33 +23,55 @@ export class FirebaseDbProvider {
 
   }
 
-  addFriendToFriendArray(friendsUserid) {
-    // let obj = {};
-    // obj[userid] = userid;
-    // this.afs.doc('users/' + this.getCurrentUser().uid).collection('friends').add(obj);
+  addFriendToFriendArray(userid) {
+    let obj = {};
+    obj[userid] = userid;
+    this.afs.doc('users/' + this.getCurrentUser().uid).collection('friends').add(obj);
 
-    let tempFriendsArray = [];
-    const friend = {
-      uid:friendsUserid
-    }
-    let dbUserRef = this.afs.collection('users').ref;
-    let user = this.getCurrentUser();
-    dbUserRef.onSnapshot(snapshot => snapshot.forEach(doc => {
-      //for each document inside of the snapshot
-      let dd = doc.data();
-      //console.log(dd);
 
-      tempFriendsArray = dd.friends;
-      console.log("tempFriendsArray:");
-      console.log(tempFriendsArray);
+    // let tempFriendsArray = [];
+    // const friend = {
+    //   uid:friendsUserid
+    // }
+    // let dbUserRef = this.afs.collection('users').ref;
+    // let user = this.getCurrentUser();
+    // dbUserRef.onSnapshot(snapshot => snapshot.forEach(doc => {
+    //   //for each document inside of the snapshot
+    //   let dd = doc.data();
+    //   //console.log(dd);
+    //
+    //   tempFriendsArray = dd.friends;
+    //   console.log("tempFriendsArray:");
+    //   console.log(tempFriendsArray);
+    //
+    //   if (dd.userid === friendsUserid) {
+    //
+    //     friend.uid = friendsUserid;
+    //   }
+    // }))
+    // dbUserRef.doc(user.uid).update({friends:[].push(friend)});
+    //
+    //
+    // let userRef = firestore().collection('users');
+    //
+    // userRef.where('name', '==', friendsUserid.value).get()
+    //   .then(snapshot => {
+    //     snapshot.forEach(doc => {
+    //       console.log(snapshot);
+    //
+    //       this.foundUser = doc.data();
+    //       this.newThread.userCol.id.push(this.foundUser.userid);
+    //       this.afs.createThread(this.newThread.userCol);
+    //       console.log(doc.id, '=>', doc.data());
+    //     });
+    //   }).catch(err => {
+    //   console.log(err)
+    // });
 
-      if (dd.userid === friendsUserid) {
 
-        friend.uid = friendsUserid;
-      }
-    }))
-    dbUserRef.doc(user.uid).update({friends:[friend]});
   }
+
+
 
   getUsers(): Observable<any> {
 
